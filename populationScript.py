23a3caf796f34dd4cade0ff2e60df12e760d8887
ibrahimeter <ibrahimeter75@gmail.com>
2021-10-03 22:32:12 +0300

    first commit

---
+++ populationScript.py
@@ -0,0 +1,24 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', "eterproject.settings")
+
+import django
+django.setup()
+
+from populationapp.models import customuser
+
+
+from faker import Faker
+
+fake = Faker()
+def fillcustomeruser(number = 100):
+        for i in range(number):
+            user = customuser.objects.get_or_create(
+                first_name= fake.name(),
+                last_name = fake.name(),
+                email = fake.email(),
+            )[0]
+
+        user.save()
+        
+if __name__ == "__main__":
+    fillcustomeruser(100)
---
+++ populationapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from populationapp.models import customuser
+
+admin.site.register(customuser)
